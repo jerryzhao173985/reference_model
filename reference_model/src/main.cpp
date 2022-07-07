4b85a0b0e6bf0d81a70a399d365b9ac2e74c29bc
Eric Kunze <eric.kunze@arm.com>
2022-07-06 17:04:05 -0700

    Update reference model version to 0.31 draft
    Include new submodule for serialization library.
    
    Change-Id: I38cdea3ed8ea3968d1dc06c689dab248d21b14fd
    Signed-off-by: Eric Kunze <eric.kunze@arm.com>

--- reference_model/src/main.cpp
+++ reference_model/src/main.cpp
@@ -26,9 +26,9 @@
 #include <nlohmann/json.hpp>
 
 #define MODEL_VERSION_MAJOR 0
-#define MODEL_VERSION_MINOR 30
+#define MODEL_VERSION_MINOR 31
 #define MODEL_VERSION_PATCH 0
-#define MODEL_VERSION_DRAFT false
+#define MODEL_VERSION_DRAFT true
 
 using namespace TosaReference;
 using namespace tosa;
