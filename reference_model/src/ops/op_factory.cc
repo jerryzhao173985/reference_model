e67115ef82bcba0718dcbd75cc8411985001b7cc
Won Jeon <won.jeon@arm.com>
2024-03-13 19:18:08 +0000

    Add missing FP8 definition and instantiation for FULLY_CONNNECTED op
    Signed-off-by: Won Jeon <won.jeon@arm.com>
    Change-Id: I7efc03f16136b2b74f8dfef79a6e7e147c93f4ed

--- reference_model/src/ops/op_factory.cc
+++ reference_model/src/ops/op_factory.cc
@@ -119,6 +119,8 @@ GraphNode* OpFactory::newOp(SubgraphTraverser* sgt,
             DEF_FACTORY_THREE_TYPE(OpFullyConnected, INT8, INT8, INT32);
             DEF_FACTORY_THREE_TYPE(OpFullyConnected, INT16, INT8, INT48);
             DEF_FACTORY_THREE_TYPE(OpFullyConnected, FP64, FP64, FP64);
+            DEF_FACTORY_THREE_TYPE(OpFullyConnected, FP8E4M3, FP8E4M3, FP16);
+            DEF_FACTORY_THREE_TYPE(OpFullyConnected, FP8E5M2, FP8E5M2, FP16);
             break;
         case Op_MATMUL:
             DEF_FACTORY_TWO_TYPE_IN_OUT(OpMatMul, FP16, FP16);
