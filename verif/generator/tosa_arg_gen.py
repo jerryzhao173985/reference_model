6cdde7e7ec25b8f36e99e386faa0ff74017a690e
Jeremy Johnson <jeremy.johnson@arm.com>
2024-08-15 13:18:08 +0100

    Add RESHAPE rank 0 tests
    Add tests for rank0->rankN and rankN->rank0.
    
    Minor refactor of test gen to always expect argument generator
    function.
    
    Merge CTS 8K RESHAPE testing into main profile testing as test
    select is done per generator call. Fixes minor issue with bool
    tests for rank 5 & 6 not being produced for fp profile.
    
    Signed-off-by: Jeremy Johnson <jeremy.johnson@arm.com>
    Change-Id: If1039abb81af988ecc1e6fe55b485d114e506f0b

--- verif/generator/tosa_arg_gen.py
+++ verif/generator/tosa_arg_gen.py
@@ -1250,9 +1250,14 @@ class TosaTensorValuesGen:
         testGen, rng, opName, dtypeList, shapeList, argsDict, error_name=None
     ):
         dtypeList[1] = DType.SHAPE
-        shapeList[1] = [len(argsDict["new_shape"])]
+        # Check for rank 0 shapes
+        size_shape = len(argsDict["new_shape"])
+        shapeList[1] = [size_shape] if size_shape > 0 else []
         # Create a new list for the pre-generated data in argsDict["fixed_data"]
-        argsDict["fixed_data"] = [None, argsDict["new_shape"]]
+        argsDict["fixed_data"] = [
+            None,
+            argsDict["new_shape"] if size_shape > 0 else [0],
+        ]
 
         return TosaTensorValuesGen.tvgLazyGenDefault(
             testGen, rng, opName, dtypeList, shapeList, argsDict, error_name
@@ -3521,8 +3526,15 @@ class TosaArgGen:
         # Find new shapes up to the number of permutations asked for
         # This code is NOT fast.  Fortunately, the numbers are fairly small.
         for p in range(testGen.args.num_rand_permutations):
-            # Rank from 1 to MAX_TENSOR_RANK
-            newRank = rng.randInt(1, (gtu.MAX_TENSOR_RANK + 1))
+            if totalElements > 1:
+                # Can't rescale to a rank 0 with more than one element
+                startRank = 1
+            else:
+                assert totalElements == 1
+                startRank = 0
+
+            # Rank from 0/1 to MAX_TENSOR_RANK
+            newRank = rng.randInt(startRank, (gtu.MAX_TENSOR_RANK + 1))
 
             # escape_counter limits the generation of new shapes to a reasonable time
             for escape_counter in range(100):
@@ -3537,7 +3549,8 @@ class TosaArgGen:
                     shuffledFactors = rng.permutation(
                         TosaArgGen.getFactors(remainingElements)
                     )
-                newShape.append(remainingElements)
+                if newRank > 0:
+                    newShape.append(remainingElements)
 
                 # Check for duplicates
                 duplicate = False
@@ -3547,7 +3560,7 @@ class TosaArgGen:
                         break
 
                 if not duplicate:
-                    outShape = "x".join([str(x) for x in newShape])
+                    outShape = testGen.shapeStr(newShape)
                     arg_list.append(
                         (
                             "perm{}_rank{}_out{}".format(p, newRank, outShape),
