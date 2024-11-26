cb04dfec68a77c58fa012213923c83481f1cf3b2
Ian Tayler Lessa <ian.taylerlessa@arm.com>
2024-11-26 13:04:07 +0000

    Add unit tests for ARGMAX FP SPECIAL
    Test behaviour with special values (max, lowest, +inf, -inf, NaN)
    Also improves the coverage of unit tests for FP special behaviour in
    CAST.
    
    Signed-off-by: Ian Tayler Lessa <ian.taylerlessa@arm.com>
    Change-Id: Iaef4a23bf4bef0550f26de9f6adb4f10e2f4efdb

--- reference_model/test/test_utils.h
+++ reference_model/test/test_utils.h
@@ -11,13 +11,22 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+#ifndef _TEST_UTILS_H
+#define _TEST_UTILS_H
 
 #include "cfloat.h"
 #include "half.hpp"
+#include "model_runner.h"
 #include "tosa_generated.h"
+
+// Remove conflicting REQUIRE definition between doctest and reference_model
+#undef REQUIRE
+#include "doctest.h"
 
 using namespace ct;
 using namespace tosa;
+using namespace TosaReference;
+using namespace half_float;
 
 template <typename T>
 constexpr DType NativeType2Dtype()
@@ -63,3 +72,144 @@ constexpr DType NativeType2Dtype()
 
     return DType_UNKNOWN;
 }
+
+/// \brief helper for building flatbuffers for unit testing purposes
+class RefModelTestBuilder
+{
+private:
+    TosaSerializationHandler _serHandler;
+    TosaSerializationRegion* _mainRegion;
+    TosaSerializationBasicBlock* _mainBlock;
+    IModelRunner _modelRunner;
+    // This will start at 0 and increase by 1 every time we use setInput.
+    int _inputsAlreadySet;
+
+public:
+    RefModelTestBuilder()
+        : RefModelTestBuilder(func_config_t{}, func_debug_t{})
+    {}
+    RefModelTestBuilder(func_config_t funcConfig, func_debug_t funcDebug)
+        : _modelRunner(funcConfig, funcDebug)
+        , _inputsAlreadySet(0)
+    {
+        // Create the main region
+        _serHandler.GetRegions().push_back(std::make_unique<TosaSerializationRegion>("region_main"));
+        _mainRegion = _serHandler.GetMainRegion();
+
+        // Create the main block
+        _mainRegion->GetBlocks().push_back(std::make_unique<TosaSerializationBasicBlock>("block_main", "region_main"));
+        _mainBlock = _mainRegion->GetBlockByName("block_main");
+    }
+
+    void addInput(std::vector<int32_t> shape, DType dtype)
+    {
+        int previousSize      = _mainBlock->GetInputs().size();
+        std::string inputName = getInputName(previousSize);
+        _mainBlock->GetInputs().push_back(inputName);
+
+        std::vector<uint8_t> data;    // empty data: these are not constants
+        _mainBlock->GetTensors().push_back(
+            std::make_unique<TosaSerializationTensor>(inputName, shape, dtype, data, false, false));
+    }
+
+    void addOutput(std::vector<int32_t> shape, DType dtype)
+    {
+        int previousSize       = _mainBlock->GetOutputs().size();
+        std::string outputName = getOutputName(previousSize);
+        _mainBlock->GetOutputs().push_back(outputName);
+
+        std::vector<uint8_t> data;    // empty data: these are not constants
+        _mainBlock->GetTensors().push_back(
+            std::make_unique<TosaSerializationTensor>(outputName, shape, dtype, data, false, false));
+    }
+
+    // addOp should be used once all inputs and outputs have already been added.
+    void addOp(Op op, Attribute attr_type, TosaAttributeBase* attr)
+    {
+        _mainBlock->GetOperators().push_back(std::make_unique<TosaSerializationOperator>(
+            op, attr_type, attr, _mainBlock->GetInputs(), _mainBlock->GetOutputs()));
+    }
+
+    void initializeRunner()
+    {
+        // Initializing with the block directly skips some extra checks like version matching,
+        // which we don't need
+        _modelRunner.initialize(*_mainBlock);
+    }
+
+    template <typename T>
+    void setInput(std::vector<T>& vals)
+    {
+        REQUIRE_MESSAGE(_inputsAlreadySet < _mainBlock->GetInputs().size(),
+                        "Error in test setup: setting more inputs than the number that were added");
+        // The vector-based call to setInput doesn't work with cfloat.h types.
+        // But the raw-ptr one does, so use that one.
+        uint8_t* data = reinterpret_cast<uint8_t*>(vals.data());
+        _modelRunner.setInput(_mainBlock->GetInputs()[_inputsAlreadySet], data, vals.size() * sizeof(T));
+        _inputsAlreadySet++;
+    }
+
+    std::string getInputName(int num)
+    {
+        return "input" + std::to_string(num);
+    }
+
+    std::string getOutputName(int num)
+    {
+        return "output" + std::to_string(num);
+    }
+
+    GraphStatus run()
+    {
+        return _modelRunner.run();
+    }
+
+    template <typename T>
+    std::vector<T> getOutput(int i, size_t size)
+    {
+        std::vector<T> output(size);
+        _modelRunner.getOutput(getOutputName(i), reinterpret_cast<uint8_t*>(output.data()), size * sizeof(T));
+        return output;
+    }
+};
+
+template <typename T>
+void compareOutput(std::vector<T>& tensor1, std::vector<T>& tensor2)
+{
+    REQUIRE_MESSAGE(tensor1.size() == tensor2.size(), "Sizes of tensor 1 and tensor2 do not match");
+    for (size_t i = 0; i < tensor1.size(); ++i)
+    {
+        CHECK_MESSAGE(tensor1[i] == doctest::Approx(tensor2[i]), "Difference in index ", i);
+    }
+}
+
+template <typename T>
+void compareOutputSpecial(std::vector<T>& tensor1, std::vector<T>& tensor2)
+{
+    REQUIRE_MESSAGE(tensor1.size() <= tensor2.size(),
+                    "Test construction error: tensor1 must not be larger than tensor2");
+    for (size_t i = 0; i < tensor1.size(); ++i)
+    {
+        T t1_i = tensor1[i];
+        T t2_i = tensor2[i];
+        if (std::isfinite(t1_i) && std::isfinite(t2_i))
+        {
+            INFO("index ", i);
+            CHECK(t1_i == t2_i);
+        }
+        else
+        {
+            INFO("index", i);
+
+            CHECK(std::isnan(t1_i) == std::isnan(t2_i));
+            // Do not check sign for NaNs
+            if (std::isnan(t1_i) && std::isnan(t2_i))
+                continue;
+
+            CHECK(std::isinf(t1_i) == std::isinf(t2_i));
+            CHECK(std::signbit(t1_i) == std::signbit(t2_i));
+        }
+    }
+}
+
+#endif
