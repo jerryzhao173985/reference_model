6b031f979b4280effba81504668cdad0d5d9f4fd
Ian Tayler Lessa <ian.taylerlessa@arm.com>
2025-02-05 10:45:22 +0000

    Add unit tests for SELECT FP special behaviour
    Signed-off-by: Ian Tayler Lessa <ian.taylerlessa@arm.com>
    Change-Id: Ia612efce3a76be90a68f5af81315f9db17096e12

--- reference_model/test/ref_model_tests.cpp
+++ reference_model/test/ref_model_tests.cpp
@@ -153,6 +153,68 @@ void testArgmaxFpSpecial(bool propagate)
     compareOutput<int32_t>(expectedOut, actualOut);
 }
 
+template <typename FP_TYPE>
+void testSelectFpSpecial()
+{
+    constexpr DType dtype           = NativeType2Dtype<FP_TYPE>();
+    constexpr TOSA_REF_TYPE refType = DType2RefType(dtype);
+
+    const int TEST_VALS = 9;
+
+    RefModelTestBuilder tb{};
+    tb.addInput({ TEST_VALS }, DType_BOOL);
+    tb.addInput({ TEST_VALS }, dtype);
+    tb.addInput({ TEST_VALS }, dtype);
+    tb.addOutput({ TEST_VALS }, dtype);
+
+    TosaSelectAttribute attr{};
+    tb.addOp(Op_SELECT, Attribute_SelectAttribute, &attr);
+
+    tb.initializeRunner();
+
+    const FP_TYPE lowest = DtypeLimits<refType>::lowest;
+    const FP_TYPE max    = DtypeLimits<refType>::max;
+    const FP_TYPE nan    = DtypeLimits<refType>::quiet_NaN;
+    // NOTE: all types supported by SELECT support inf
+    const FP_TYPE inf = DtypeLimits<refType>::infinity;
+
+    const int8_t SELECT_INPUT2 = 1;
+    const int8_t SELECT_INPUT3 = 0;
+
+    // Alternate between selecting from input2 and input3
+    std::vector<int8_t> selection = {
+        SELECT_INPUT2, SELECT_INPUT3, SELECT_INPUT2, SELECT_INPUT3, SELECT_INPUT2,
+        SELECT_INPUT3, SELECT_INPUT2, SELECT_INPUT3, SELECT_INPUT2,
+    };
+    std::vector<FP_TYPE> inVals2 = {
+        FP_TYPE(0), lowest, inf, -inf, -inf, FP_TYPE(2), max, nan, nan,
+    };
+    std::vector<FP_TYPE> inVals3 = {
+        FP_TYPE(2), lowest, inf, -inf, inf, lowest, FP_TYPE(0), FP_TYPE(2), max,
+    };
+
+    std::vector<FP_TYPE> expectedOut = {
+        FP_TYPE(0),    // 2
+        lowest,        // 3
+        inf,           // 2
+        -inf,          // 3
+        -inf,          // 2
+        lowest,        // 3
+        max,           // 2
+        FP_TYPE(2),    // 3
+        nan,           // 2
+    };
+
+    tb.setInput(selection);
+    tb.setInput(inVals2);
+    tb.setInput(inVals3);
+
+    REQUIRE(tb.run() == GraphStatus::TOSA_VALID);
+    std::vector<FP_TYPE> actualOut = tb.getOutput<FP_TYPE>(0, /* size */ expectedOut.size());
+
+    compareOutputSpecial<FP_TYPE>(expectedOut, actualOut);
+}
+
 void testConv2d(std::vector<int8_t>& inVals, std::vector<int8_t>& weightVals, int8_t inZp, int8_t weightZp)
 {
     RefModelTestBuilder tb{};
@@ -704,5 +766,10 @@ TEST_SUITE("reference_model")
             // 55 == std::bitcast<int8_t>(uint8_t(0x37))
             testCast<int16_t, int8_t>(-28617, 55);
         }
+    }
+
+    TEST_CASE_TEMPLATE("SELECT FP SPECIAL", IN_TYPE, float, half, bfloat16)
+    {
+        testSelectFpSpecial<IN_TYPE>();
     }
 }    // TEST_SUITE("reference_model")
